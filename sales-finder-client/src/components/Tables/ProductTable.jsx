572fed325d3774ddcaec1f8ab08d227811864e26
harryhii <hartwig.hii@gmail.com>
2020-01-13 15:44:45 +0200

    [HH-17] create initial (functional) demo

--- sales-finder-client/src/components/Tables/ProductTable.jsx
+++ sales-finder-client/src/components/Tables/ProductTable.jsx
@@ -99,45 +99,9 @@ export default class ProductTable extends React.Component {
       window.open('https://www.google.com', '_blank');
     }
 
-    function perc2color(perc,min,max) {
-      var base = (max - min);
-
-      if (base == 0) { perc = 100; }
-      else {
-          perc = (perc - min) / base * 100; 
-      }
-      var r, g, b = 0;
-      if (perc < 50) {
-          r = 255;
-          g = Math.round(5.1 * perc);
-      }
-      else {
-          g = 255;
-          r = Math.round(510 - 5.10 * perc);
-      }
-      var h = r * 0x10000 + g * 0x100 + b * 0x1;
-      
-      return '#' + ('000000' + h.toString(16)).slice(-6);
-  }
-
-  function getPrecentageMax(data) {
-    let precentageArr = [];
-    for (let i = 0; i < data.length; i++) {
-      precentageArr.push(data[i].precentage);
-    }
-    return Math.max(...precentageArr);
-  }
-
-  function getPrecentageMin(data) {
-    let precentageArr = [];
-    for (let i = 0; i < data.length; i++) {
-      precentageArr.push(data[i].precentage);
-    }
-    return Math.min(...precentageArr);
-  }
     const precentageFormatter = (cell, row) => {
         return (
-          <span style={ { color: perc2color(row.precentage, getPrecentageMin(data), getPrecentageMax(data)),  'text-shadow': '1px 1px 1px #7A7A7A' } }>
+          <span>
             { cell }%
           </span>
         );
