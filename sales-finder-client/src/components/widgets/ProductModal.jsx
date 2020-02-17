ec136a1dd53526c96dd54b71ee0cddf55c4ecf3f
Hartwig <hartwig.hii@gmail.com>
2020-02-17 16:12:01 +0200

    [HH-802] Modal price chart

--- sales-finder-client/src/components/widgets/ProductModal.jsx
+++ sales-finder-client/src/components/widgets/ProductModal.jsx
@@ -1,12 +1,15 @@
 import React from 'react';
-
-import Chart from "chart.js";
+import { Line } from 'react-chartjs-2';
 
 /**stateful component */
 export default class ProductModal extends React.Component {
 
     constructor(props) {
         super(props);
+
+        this.state = {
+            
+        }
     }
 
 
@@ -14,8 +17,6 @@ export default class ProductModal extends React.Component {
 
         const productId = this.props.productId;
         const data = this.props.data;
-
-        console.log('current id is ', productId);
 
         function filterId(data, productId) {
             
@@ -31,7 +32,24 @@ export default class ProductModal extends React.Component {
 
         let foundData = filterId(data, productId);
 
-        console.log(foundData);
+        let foundDataLabels = foundData.price.map(price => price.date);
+        let foundDataSets = foundData.price.map(price => price.price);
+
+        let test = {
+            labels: foundDataLabels,
+            datasets: [
+                {
+                    label:"Price over time",
+                    data: foundDataSets,
+                    fill: false,
+                    borderColor: "#17a2b8"
+                }
+            ],            
+        }
+
+        let calculateSavePrecentage = (((foundData.price[0].price - foundData.price[(foundData.price.length -1)].price) 
+        * 100) / foundData.price[0].price);
+
 
         return (
             <div className="modal fade" id="exampleModalCenter" tabIndex="-1" role="dialog" aria-labelledby="exampleModalCenterTitle" aria-hidden="true" style={{display: "none"}}>
@@ -49,24 +67,23 @@ export default class ProductModal extends React.Component {
                                     <a href="#"><img src={foundData.productImgUrl} alt="Monton hooded top" className="active" height="290" /></a>
                                 </div>
                                 <div className="col-9">
-                                    <div className="card-header">
-                                        <i className="fas fa-chart-area"></i>
-                                        product price over time</div>
-                                    <div className="card-body"><div className="chartjs-size-monitor"><div className="chartjs-size-monitor-expand"><div className=""></div></div><div className="chartjs-size-monitor-shrink"><div className=""></div></div></div>
-                                        <canvas id="myAreaChart" width="527" height="158" style={{display: "block", width: "527px", height: "158px"}} className="chartjs-render-monitor"></canvas>
-                                    </div>
-                                    <div className="card-footer small text-muted">Updated yesterday at 11:59 PM</div>
+                                    <div className="card-body">
+                                        <Line
+                                            options={{responsive:true}}
+                                            data={test}
+                                        />
+                                    </div>    
+                                        <div className="card-footer small text-muted">Updated at {foundData.price[(foundData.price.length -1)].date}</div>
 
                                 </div>
                                 <div className="col-12">
                                     <hr />
-                                    <p className="text-center align-middle">Product starting price: {foundData.price[0].price} - Product price now: {foundData.price[0].price}<b className="text-success"></b> - Total % drop: <b className="text-success"></b></p>
+                                    <p className="text-center align-middle">Product starting price: {foundData.price[0].price} - Product price now: {foundData.price[(foundData.price.length -1)].price}<b className="text-success"></b> - Total save: {calculateSavePrecentage} %<b className="text-success"></b></p>
                                 </div>
                             </div>
                         </div>
                         <div className="modal-footer">
                             <button type="button" className="btn btn-secondary" data-dismiss="modal">Close</button>
-                            <button type="button" className="btn btn-primary">Add to wishlist +</button>
                         </div>
                     </div>
                 </div>
